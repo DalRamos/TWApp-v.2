07950a97bcdfca19397f170f078eacc9ff7832d4
JamesRapelo <jrapelo25@gmail.com>
2023-10-21 08:48:12 +0800

    Implemented Splash Screen

--- app/src/main/java/com/oop/twapp/MainActivity.kt
+++ app/src/main/java/com/oop/twapp/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.findNavController
 
@@ -11,6 +12,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Thread.sleep(3000)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
 
     }
