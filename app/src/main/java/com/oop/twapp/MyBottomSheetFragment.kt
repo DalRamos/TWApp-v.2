b968f1483c6df045f82119aef2595c629b54af52
Dalrymple Ramos <d4rukun@gmail.com>
2023-10-21 14:31:06 +0800

    updated bottomsheetlayout, ftcards funtionality, reservationfragment, navgraph

--- app/src/main/java/com/oop/twapp/MyBottomSheetFragment.kt
+++ app/src/main/java/com/oop/twapp/MyBottomSheetFragment.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class MyBottomSheetFragment : BottomSheetDialogFragment() {
+
+    private lateinit var confirmButton: Button
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.bottom_sheet_layout, container, false)
+
+        val confirmButton = view.findViewById<Button>(R.id.confirmButton)
+
+        confirmButton.setOnClickListener {
+
+        }
 
 
 
