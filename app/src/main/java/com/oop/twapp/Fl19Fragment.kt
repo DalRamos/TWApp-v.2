2aaa21431eb686312d31c04a0d44f32a6a3abdf3
Andrei <ocampojamesandrei@gmail.com>
2023-10-22 14:50:47 +0800

    ForeignLanguages Fragments and Tutors

---
+++ app/src/main/java/com/oop/twapp/Fl19Fragment.kt
@@ -0,0 +1,40 @@
+package com.oop.twapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.oop.twapp.databinding.FragmentFl19Binding
+
+
+class Fl19Fragment : Fragment() {
+
+    private lateinit var binding: FragmentFl19Binding
+    private lateinit var appointmentButton: Button
+    private lateinit var flback: ImageView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View{
+        binding = FragmentFl19Binding.inflate(inflater, container, false)
+
+        appointmentButton = binding.root.findViewById(R.id.appointmentButton)
+        flback = binding.root.findViewById(R.id.flback)
+
+        appointmentButton.setOnClickListener {
+            val bottomSheetFragment = MyBottomSheetFragment()
+            bottomSheetFragment.show(childFragmentManager, bottomSheetFragment.tag)
+        }
+
+        flback.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        return binding.root
+    }
+}
