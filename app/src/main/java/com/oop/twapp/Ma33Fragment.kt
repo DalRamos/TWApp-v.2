bde0985e471e004572d641aa564851d62a499bd8
Andrei <ocampojamesandrei@gmail.com>
2023-10-22 19:05:53 +0800

    Music and Arts Fragments and Tutors

---
+++ app/src/main/java/com/oop/twapp/Ma33Fragment.kt
@@ -0,0 +1,40 @@
+package com.oop.twapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.oop.twapp.databinding.FragmentMa33Binding
+
+
+class Ma33Fragment : Fragment() {
+
+    private lateinit var binding: FragmentMa33Binding
+    private lateinit var appointmentButton: Button
+    private lateinit var maback: ImageView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View{
+        binding = FragmentMa33Binding.inflate(inflater, container, false)
+
+        appointmentButton = binding.root.findViewById(R.id.appointmentButton)
+        maback = binding.root.findViewById(R.id.maback)
+
+        appointmentButton.setOnClickListener {
+            val bottomSheetFragment = MyBottomSheetFragment()
+            bottomSheetFragment.show(childFragmentManager, bottomSheetFragment.tag)
+        }
+
+        maback.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        return binding.root
+    }
+}
